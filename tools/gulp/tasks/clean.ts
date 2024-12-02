3778e7499c1fe3b1b83d139348a317e53b281075
lanbery <cwd-pc@xy-info.com>
2024-12-02 20:28:19 +0800

    ci: ignore

--- tools/gulp/tasks/clean.ts
+++ tools/gulp/tasks/clean.ts
@@ -1,6 +1,11 @@
 import { src, task } from 'gulp';
 import * as clean from 'gulp-clean';
-import { sources } from '../config';
+
+import { sources,dist } from '../config';
+import * as rimraf from 'rimraf'
+import path = require('path');
+
+
 
 /**
  * Cleans the build output assets from the src folders
@@ -18,4 +23,12 @@ function cleanSrc() {
   }).pipe(clean());
 }
 
-task('clean:src', cleanSrc);
+function cleanDist(){
+  const files = dist.map(folder => 
+    `${folder}/*`
+  );
+  return src(files,{read:false}).pipe(clean())
+}
+
+task('clean:dist',cleanDist);
+task('clean:src',cleanSrc);
